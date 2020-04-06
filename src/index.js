d9211b566fa84671fd41f8465fe8651e1c11027e
Tyler Alder <tyler.b.alder@gmail.com>
2020-04-06 15:34:40 -0700

    lots o stuff

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from "./App";
+import "./index.css";
+import RouletteGun from "./state-drills/RouletteGun";
+// import HelloWorld from "./state-drills/HelloWorld";
+// import Bomb from "./state-drills/Bomb";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+ReactDOM.render(<RouletteGun />, document.getElementById("root"));
+// ReactDOM.render(<Bomb />, document.getElementById("root"));
---
+++ src/state-drills/HelloWorld.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class HelloWorld extends React.Component {
+  state = {
+    who: "World",
+  };
+
+  render() {
+    return (
+      <div className="HelloWorld">
+        <p>Hello {this.state.who}!</p>
+        <button onClick={() => this.setState({ who: "World" })}>World</button>
+        <button onClick={() => this.setState({ who: "Friend" })}>Friend</button>
+        <button onClick={() => this.setState({ who: "React" })}>React</button>
+      </div>
+    );
+  }
+}
