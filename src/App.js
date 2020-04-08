4edd19a99bbf213b5e3aec56c563340a8c26fd4d
Tyler Alder <tyler.b.alder@gmail.com>
2020-04-08 05:14:46 -0700

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from "react";
 import "./App.css";
-import TheDate from "./state/TheDate";
-import Messages from "./Messages";
-import Counter from "./state/Counter";
+// import TheDate from "./state/TheDate";
+// import Messages from "./Messages";
+// import Counter from "./state/Counter";
+import Tabs from "./state/Tabs";
+
+const tabsProp = [
+  {
+    name: "First tab",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.",
+  },
+  {
+    name: "Second tab",
+    content:
+      "Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    name: "Third tab",
+    content:
+      "Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.",
+  },
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter count={123} />
+        <Tabs tabs={tabsProp} />
+        {/* <Counter count={123} />
         <TheDate />
         <Messages name="Messages" unread={0} />
         <Messages name="Notifications" unread={10} />
         <h1>YOUR APPLICATION NAME!</h1>
-        <div>Hello!</div>
+        <div>Hello!</div> */}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import App from "./App";
+import App from "./App";
 import "./index.css";
-import RouletteGun from "./state-drills/RouletteGun";
+// import RouletteGun from "./state-drills/RouletteGun";
 // import HelloWorld from "./state-drills/HelloWorld";
 // import Bomb from "./state-drills/Bomb";
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 // ReactDOM.render(<HelloWorld />, document.getElementById("root"));
-ReactDOM.render(<RouletteGun />, document.getElementById("root"));
+// ReactDOM.render(<RouletteGun />, document.getElementById("root"));
 // ReactDOM.render(<Bomb />, document.getElementById("root"));
