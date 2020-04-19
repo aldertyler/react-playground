7ee5bfa3ff0a9307794082ebbfcc6d8eb3a59a7c
Tyler Alder <tyler.b.alder@gmail.com>
2020-04-19 09:34:04 -0700

    lots of new playthings

--- src/App.js
+++ src/App.js
@@ -1,57 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
-// import TheDate from "./state/TheDate";
-// import Messages from "./Messages";
-// import Counter from "./state/Counter";
-// import Tabs from "./state/Tabs";
-import Accordion from "./state-drills/Accordion";
 
-// const tabsProp = [
-//   {
-//     name: "First tab",
-//     content:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.",
-//   },
-//   {
-//     name: "Second tab",
-//     content:
-//       "Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-//   },
-//   {
-//     name: "Third tab",
-//     content:
-//       "Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.",
-//   },
-// ];
-const sections = [
-  {
-    title: "Section 1",
-    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  },
-  {
-    title: "Section 2",
-    content:
-      "Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!",
-  },
-  {
-    title: "Section 3",
-    content:
-      "Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?",
-  },
-];
+import DemonynApp from "./demonymapp/demonymApp";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Accordion sections={sections} />
-        {/* <Tabs tabs={tabsProp} /> */}
-        {/* <Counter count={123} />
-        <TheDate />
-        <Messages name="Messages" unread={0} />
-        <Messages name="Notifications" unread={10} />
-        <h1>YOUR APPLICATION NAME!</h1>
-        <div>Hello!</div> */}
+        <DemonynApp />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import AppLang from "./lang-context/AppLang";
 import "./index.css";
-// import RouletteGun from "./state-drills/RouletteGun";
-// import HelloWorld from "./state-drills/HelloWorld";
-// import Bomb from "./state-drills/Bomb";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-// ReactDOM.render(<HelloWorld />, document.getElementById("root"));
-// ReactDOM.render(<RouletteGun />, document.getElementById("root"));
-// ReactDOM.render(<Bomb />, document.getElementById("root"));
+ReactDOM.render(<AppLang />, document.getElementById("root"));
